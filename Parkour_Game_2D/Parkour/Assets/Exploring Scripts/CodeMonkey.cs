918e6673d9443912eb542b385acee20a0de8098c
Ashutosh Sharma <sharma.aashu1208@gmail.com>
2024-08-16 23:52:37 +0530

    revision ds and design pattern

---
+++ Parkour_Game_2D/Parkour/Assets/Exploring Scripts/CodeMonkey.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CodeMonkey : MonoBehaviour
+{
+
+    enum PlayerAction
+    {
+        a,
+        b,
+        c,
+        d
+
+    }
+
+    PlayerAction PA()
+    {
+
+        return PlayerAction.a;
+        Debug.Log("");
+
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
