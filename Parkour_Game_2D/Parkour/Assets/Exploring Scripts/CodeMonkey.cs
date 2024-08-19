03cc0eb67d0caf543561253d9a0b6f8af0ca3bd1
Ashutosh Sharma <sharma.aashu1208@gmail.com>
2024-08-19 23:38:21 +0530

    stack, queue

--- Parkour_Game_2D/Parkour/Assets/Exploring Scripts/CodeMonkey.cs
+++ Parkour_Game_2D/Parkour/Assets/Exploring Scripts/CodeMonkey.cs
@@ -8,10 +8,19 @@ public class CodeMonkey : MonoBehaviour
     public Dictionary<string, int> dict = new Dictionary<string, int>();
     public List<string> list = new List<string>();
     public int[] arr;
+    public Stack<string> stackString = new Stack<string>();
+    public Queue<string> queueString = new Queue<string>();
+    public HashSet<string> hashsetString = new HashSet<string>();
     private void Awake()
     {
 
-
+        stackString.Push("codemonkey");
+        stackString.Push("aashu hindi games");
+        queueString.Enqueue("1");
+        queueString.Enqueue("2");
+        //stackString.Pop();
+        //Debug.Log(stackString.Pop());
+        Debug.Log(queueString.Dequeue());
     }
 
     // Start is called before the first frame update
