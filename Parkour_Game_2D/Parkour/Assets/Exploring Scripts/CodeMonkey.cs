8dd1b87a518787721cd4e0e495e77ce3dfce676d
Ashutosh Sharma <sharma.aashu1208@gmail.com>
2024-08-19 22:53:41 +0530

    dictionary

--- Parkour_Game_2D/Parkour/Assets/Exploring Scripts/CodeMonkey.cs
+++ Parkour_Game_2D/Parkour/Assets/Exploring Scripts/CodeMonkey.cs
@@ -5,66 +5,19 @@ using UnityEngine;
 public class CodeMonkey : MonoBehaviour
 {
     public static CodeMonkey instance { get; private set; }
-
+    public Dictionary<string, int> dict = new Dictionary<string, int>();
+    public List<string> list = new List<string>();
+    public int[] arr;
     private void Awake()
     {
 
-        if (instance != null)
-        {
-            //instance = this;
-            Debug.LogError("Instance already exist");
-            Destroy(gameObject);
-            return;
-        }
-        instance = this;
-      /*  else
-        {
-            DontDestroyOnLoad(this);
-        }
-      */
-    }
-
-    /* enum PlayerAction
-     {
-         a,
-         b,
-         c,
-         d
-
-     }*/
-
-    /*PlayerAction PA()
-    {
-
-        return PlayerAction.a;
-        Debug.Log("");
 
     }
-*/
+
     // Start is called before the first frame update
     void Start()
     {
         //SetNumber();
     }
-    /*public int RecursionIncrementer(int number)
-    {
-        if (number<5)
-        {
-            return number+1;
-        }
-        else
-        {
-            return number - 1;
-        }
-
-        
-    }
-
-    public void SetNumber()
-    {
-
-        
-        Debug.Log(RecursionIncrementer(6));
-
-    }*/
+    
 }
