06e179c1e602777e6ddad304ceaf06bb22357b3f
Ashutosh Sharma <sharma.aashu1208@gmail.com>
2024-08-20 22:30:38 +0530

    updated

--- Parkour_Game_2D/Parkour/Assets/Exploring Scripts/Exploring_Revisions.cs
+++ Parkour_Game_2D/Parkour/Assets/Exploring Scripts/Exploring_Revisions.cs
@@ -19,19 +19,34 @@ public class Exploring_Revisions : MonoBehaviour
 
     public void Set_Pool()
     {
-
+        for (int i = 0; i < poolSize; i++)
+        {
+            GameObject obj = Instantiate(prefabs);
+            obj.SetActive(false);
+            pool.Add(obj);
+        }
 
     }
 
     public GameObject Get_Pool()
     {
 
+        for (int i = 0; i < pool.Count; i++)
+        {
+            if (!pool[i].activeInHierarchy)
+            {
+                pool[i].SetActive(true);
+                return pool[i];
+            }
 
+        }
+        return null;
     }
 
-    IEnumerator Return_To_Pool()
+    IEnumerator Return_To_Pool(GameObject obj)
     {
-
+        yield return new WaitForSeconds(3f);
+        obj.SetActive(false);
 
     }
 }
